1600dbf64ddcdfc7c2aefba66856d4e68615553d
TAZZ <42774502+tazz509@users.noreply.github.com>
2019-04-01 13:26:53 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,21 +8,4 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require './app/controllers/application_controller'
 require_all 'app'
-
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :name     => db.name,
-    :username => db.username,
-    :email    => db.email,
-    :city     => db.city,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-    )
-end
